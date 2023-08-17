27d2780a28a24ef93c3627939cfaeff0d0886f6d
musagenius345 <musabadru@gmail.com>
2023-08-18 02:25:12 +0300

    still buggy localStorage items can not be deleted by delete button

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
-// Get elements from the DOM
-const todoInput = document.getElementById('todo-input');
 const addButton = document.getElementById('add-button');
+const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 const modeBtn = document.querySelector('.mode');
 const htmlElement = document.querySelector('html');
-let currentTheme = htmlElement.getAttribute('data-theme')
+let currentTheme = htmlElement.getAttribute('data-theme');
+
+const storedTheme = localStorage.getItem('toDoTheme');
+if (storedTheme) {
+  currentTheme = storedTheme;
+  htmlElement.setAttribute('data-theme', currentTheme);
+  updateIcons(currentTheme);
+}
 
 function themeToggle() {
-  currentTheme = currentTheme === 'dark' ? 'light' : 'dark'
+  currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
   htmlElement.setAttribute('data-theme', currentTheme);
+  localStorage.setItem('toDoTheme', currentTheme);
+  updateIcons(currentTheme);
 }
 
 function updateIcons(theme) {
   const moonIcon = theme === 'dark' ? 'none' : 'inline';
   const sunIcon = theme === 'light' ? 'none' : 'inline';
-
   const moonIconElement = document.querySelector('.fa-moon');
   const sunIconElement = document.querySelector('.fa-sun');
-
   moonIconElement.style.display = moonIcon;
   sunIconElement.style.display = sunIcon;
 }
 
-
 function handleButtons() {
   const todoText = todoInput.value.trim();
-
   if (todoText !== '') {
-    // Create a new todo item
     const todoItem = document.createElement('li');
     todoItem.innerHTML = `
-                <span class="todo-content">${todoText}</span>
-                <button class="delete-button">Delete</button>
-              `;
-    
-
-    // Add event listener to the "Delete" button
+      <div class="todo-content"><p>${todoText}</p></div>
+      <button class="delete-button">Delete</button>
+    `;
     const deleteButton = todoItem.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => {
       todoItem.remove();
+      localStorage.removeItem('todos');
     });
-    // Append the new todo item to the list
     todoList.appendChild(todoItem);
-
-    // Clear the input field
+    localStorage.setItem('todos', todoList.innerHTML);
     todoInput.value = '';
   }
-  
 }
 
-
-// Add event listener to the "Add" button
-addButton.addEventListener('click', handleButtons)
+addButton.addEventListener('click', handleButtons);
+todoInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    handleButtons();
+  }
+});
 modeBtn.addEventListener('click', themeToggle);
+
+// Restore todos from local storage on page load
+const storedTodos = localStorage.getItem('todos');
+if (storedTodos) {
+  todoList.innerHTML = storedTodos;
+}
