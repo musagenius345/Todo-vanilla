d8e680a20da1ce142dbbdcbff862ae60685d9418
musagenius345 <musabadru@gmail.com>
2023-08-18 01:25:26 +0300

    Light mode toggle

--- main.js
+++ main.js
@@ -2,20 +2,38 @@
 const todoInput = document.getElementById('todo-input');
 const addButton = document.getElementById('add-button');
 const todoList = document.getElementById('todo-list');
+const modeBtn = document.querySelector('.mode');
+const htmlElement = document.querySelector('html');
+let currentTheme = htmlElement.getAttribute('data-theme')
 
-// Add event listener to the "Add" button
-addButton.addEventListener('click', () => {
-  // Get the value from the input field
+function themeToggle() {
+  currentTheme = currentTheme === 'dark' ? 'light' : 'dark'
+  htmlElement.setAttribute('data-theme', currentTheme);
+}
+
+function updateIcons(theme) {
+  const moonIcon = theme === 'dark' ? 'none' : 'inline';
+  const sunIcon = theme === 'light' ? 'none' : 'inline';
+
+  const moonIconElement = document.querySelector('.fa-moon');
+  const sunIconElement = document.querySelector('.fa-sun');
+
+  moonIconElement.style.display = moonIcon;
+  sunIconElement.style.display = sunIcon;
+}
+
+
+function handleButtons() {
   const todoText = todoInput.value.trim();
 
   if (todoText !== '') {
     // Create a new todo item
     const todoItem = document.createElement('li');
-    todoItem.classList.add('todo-item');
     todoItem.innerHTML = `
-              <span>${todoText}</span>
-              <button class="delete-button">Delete</button>
-            `;
+                <span class="todo-content">${todoText}</span>
+                <button class="delete-button">Delete</button>
+              `;
+    
 
     // Add event listener to the "Delete" button
     const deleteButton = todoItem.querySelector('.delete-button');
@@ -28,4 +46,10 @@ addButton.addEventListener('click', () => {
     // Clear the input field
     todoInput.value = '';
   }
-});
+  
+}
+
+
+// Add event listener to the "Add" button
+addButton.addEventListener('click', handleButtons)
+modeBtn.addEventListener('click', themeToggle);
